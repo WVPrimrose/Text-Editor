e90227d445950e68c333e16ab39df4cd166d43b2
Katie Buchholz <kzbindy@hotmail.com>
2024-07-17 23:10:02 -0400

    cleared comments out of the database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,19 +14,14 @@ const initdb = async () => openDB('textEditor', 1, {
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
 
-  // Create a connection to the database database and version we want to use.
   const textEditorDb = await openDB('textEditor', 1);
 
-  // Create a new transaction and specify the database and data privileges.
   const tx = textEditorDb.transaction('textEditor', 'readwrite');
 
-  // Open up the desired object store.
   const store = tx.objectStore('textEditor');
 
-  // Use the .add() method on the store and pass in the content.
   const request = store.put({id:1, value:content});
 
-  // Get confirmation of the request.
   const result = await request;
   console.log('Data saved to the database', result);
 };
@@ -35,19 +30,14 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('Post to the database');
 
-  // Create a connection to the database database and version we want to use.
   const textEditorDb = await openDB('textEditor', 1);
 
-  // Create a new transaction and specify the database and data privileges.
   const tx = textEditorDb.transaction('textEditor', 'readonly');
 
-  // Open up the desired object store.
   const store = tx.objectStore('textEditor');
 
-  // Use the .add() method on the store and pass in the content.
   const request = store.get(1);
 
-  // Get confirmation of the request.
   const result = await request;
   console.log('Data saved to the database', result);
   return result?.vaule;
