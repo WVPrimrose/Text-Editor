d3723f9f48b9e32a55d6ba055c5f5b91a758cb9d
Katie Buchholz <kzbindy@hotmail.com>
2024-07-17 23:05:28 -0400

    finally got the app and database running

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,33 +1,30 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('text-editor', 1, {
+const initdb = async () => openDB('textEditor', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('text-editor')) {
+      if (db.objectStoreNames.contains('textEditor')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('text-editor', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('textEditor', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-{
-  console.log('Post to the database');
+export const putDb = async (content) => {
 
   // Create a connection to the database database and version we want to use.
-  const textEditorDb = await openDB('text-editor', 1);
+  const textEditorDb = await openDB('textEditor', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = textEditorDb.transaction('text-editor', 'readwrite');
+  const tx = textEditorDb.transaction('textEditor', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('text-editor');
+  const store = tx.objectStore('textEditor');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add(content);
+  const request = store.put({id:1, value:content});
 
   // Get confirmation of the request.
   const result = await request;
@@ -39,21 +36,21 @@ export const getDb = async () => {
   console.log('Post to the database');
 
   // Create a connection to the database database and version we want to use.
-  const textEditorDb = await openDB('text-editor', 1);
+  const textEditorDb = await openDB('textEditor', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = textEditorDb.transaction('text-editor', 'readonly');
+  const tx = textEditorDb.transaction('textEditor', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('text-editor');
+  const store = tx.objectStore('textEditor');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('Data saved to the database', result);
-  return result;
+  return result?.vaule;
 };
 
 initdb();
